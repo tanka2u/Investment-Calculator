03029ccc98c0e885eff849529044352f51c10768
tanka2u <ptanka123@gmail.com>
2024-03-07 19:40:52 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from "../assets/investment-calculator-logo.png";
+
+export default function Header() {
+  return (
+    <header id="header">
+      <img id="img" src={Logo} alt="Logo showing a money bag" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
